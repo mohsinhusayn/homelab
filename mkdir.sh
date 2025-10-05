1843e59e45cd454f2536adddde8b4b282b792f9a
Mohsin <husaynmohsin@gmail.com>
2025-10-05 18:24:27 +0400

    Script to make volume directories

--- mkdir.sh
+++ mkdir.sh
@@ -6,36 +6,36 @@
 
 # Create directories for Cloud stack
 mkdir -p \
-  /home/mohsin/Mohsin/git/homelab/stacks/cloud/nextcloud/{data,config,apps,db_data} \
-  /home/mohsin/Mohsin/git/homelab/stacks/cloud/immich/{library,postgres}
+  /home/username/homelab/stacks/cloud/nextcloud/{data,config,apps,db_data} \
+  /home/username/homelab/stacks/cloud/immich/{library,postgres}
 
 # Create directories for Media stack
 mkdir -p \
-  /home/mohsin/Mohsin/git/homelab/stacks/media/jellyfin/{config,logs,data} \
-  /home/mohsin/Mohsin/git/homelab/stacks/media/sonarr/{config,logs} \
-  /home/mohsin/Mohsin/git/homelab/stacks/media/radarr/{config,logs} \
-  /home/mohsin/Mohsin/git/homelab/stacks/media/bazarr/{config,logs} \
-  /home/mohsin/Mohsin/git/homelab/stacks/media/prowlarr/{config,logs} \
-  /home/mohsin/Mohsin/git/homelab/stacks/media/qbittorrent/{config,downloads,logs} \
-  /home/mohsin/Mohsin/git/homelab/media/{movies,tvs} \
-  /home/mohsin/Mohsin/git/homelab/qbittorrent/downloads
+  /home/username/homelab/stacks/media/jellyfin/{config,logs,data} \
+  /home/username/homelab/stacks/media/sonarr/{config,logs} \
+  /home/username/homelab/stacks/media/radarr/{config,logs} \
+  /home/username/homelab/stacks/media/bazarr/{config,logs} \
+  /home/username/homelab/stacks/media/prowlarr/{config,logs} \
+  /home/username/homelab/stacks/media/qbittorrent/{config,downloads,logs} \
+  /home/username/homelab/media/{movies,tvs} \
+  /home/username/homelab/qbittorrent/downloads
 
 # Create directories for Monitor stack
 mkdir -p \
-  /home/mohsin/Mohsin/git/homelab/stacks/monitor/kuma/{data,logs} \
-  /home/mohsin/Mohsin/git/homelab/stacks/monitor/grafana/{data,logs} \
-  /home/mohsin/Mohsin/git/homelab/stacks/monitor/prometheus/{config,logs} \
-  /home/mohsin/Mohsin/git/homelab/stacks/monitor/cadvisor/logs
+  /home/username/homelab/stacks/monitor/kuma/{data,logs} \
+  /home/username/homelab/stacks/monitor/grafana/{data,logs} \
+  /home/username/homelab/stacks/monitor/prometheus/{config,logs} \
+  /home/username/homelab/stacks/monitor/cadvisor/logs
 
 # Create directories for Security stack
 mkdir -p \
-  /home/mohsin/Mohsin/git/homelab/stacks/security/npm/{data,letsencrypt}
+  /home/username/homelab/stacks/security/npm/{data,letsencrypt}
 
 # Create directories for Admin stack
 mkdir -p \
-  /home/mohsin/Mohsin/git/homelab/stacks/admin/homarr/{appdata,icons,logs} \
-  /home/mohsin/Mohsin/git/homelab/stacks/admin/filebrowser/{config,database,logs} \
-  /home/mohsin/Mohsin/git/homelab/stacks/admin/filebrowser/root
+  /home/username/homelab/stacks/admin/homarr/{appdata,icons,logs} \
+  /home/username/homelab/stacks/admin/filebrowser/{config,database,logs} \
+  /home/username/homelab/stacks/admin/filebrowser/root
 
 echo "Directories created successfully."
 
